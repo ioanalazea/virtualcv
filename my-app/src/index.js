dff6fa59f62c8deea9425a60da21a4fa71811a07
Ioana Lazea <lazeaioanabianca@gmail.com>
2023-09-23 02:15:09 +0300

    updates

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from 'react-dom';
+//import RouterMapping from './RouterMapping';
+const RouterMapping = () => (
+  <Router>
+   <Routes>
+      <Route exact path="/virtualcv" element={<App />} />
+      {/* <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} /> */}
+    </Routes>
+  </Router>
+);
+
+ReactDOM.render(
+  <RouterMapping />, 
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
